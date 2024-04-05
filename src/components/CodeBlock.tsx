a05ce05cd60b09901007523e902d17aff5689be9
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-05 19:48:28 +0200

    Add babel plugin, configure prismjs, tune text spacing

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,8 +1,8 @@
 import { Copy, LucideIcon } from 'lucide-react'
 import Prism from 'prismjs'
 import { PropsWithChildren, useEffect } from 'react'
-import Link from './Link.tsx'
 import ActionButton from './ActionButton.tsx'
+import Link from './Link.tsx'
 
 function copyButtonClick(code: string) {
   navigator.clipboard.writeText(code)
@@ -12,7 +12,7 @@ const CodeBlock = ({
   codeToCopy = '',
   children,
   Icon = undefined,
-  language = 'javascript',
+  language = 'js',
   linkHref = '',
   linkText = '',
 }: PropsWithChildren<CodeProps>) => {
@@ -22,11 +22,13 @@ const CodeBlock = ({
 
   return (
     <div className={'relative mb-4 rounded-lg border border-zinc-700 bg-slate-800 text-zinc-300'}>
-      <code
-        className={`language-${language} block overflow-y-auto whitespace-pre rounded-lg bg-transparent p-4 text-sm text-zinc-300`}
-      >
-        {children}
-      </code>
+      <pre className="line-numbers">
+        <code
+          className={`language-${language} block overflow-y-auto whitespace-pre rounded-lg bg-transparent p-4 text-sm text-zinc-300`}
+        >
+          {children}
+        </code>
+      </pre>
       {codeToCopy && (
         <div className="absolute right-3 top-3">
           <ActionButton
@@ -56,7 +58,7 @@ export interface CodeProps {
   children: PropsWithChildren
   codeToCopy?: string | undefined
   Icon?: LucideIcon | undefined
-  language?: 'javascript' | 'css' | 'html' | 'tsx'
+  language?: 'js' | 'css' | 'html' | 'tsx' | 'markup'
   linkHref?: string | undefined
   linkText?: string | undefined
 }
