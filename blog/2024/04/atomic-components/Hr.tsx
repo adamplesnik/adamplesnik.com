56fbeae78832a6efead6e685fc6edc432f8518f9
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-16 20:52:41 +0200

    Update atomic example

--- blog/2024/04/atomic-components/Hr.tsx
+++ blog/2024/04/atomic-components/Hr.tsx
@@ -3,18 +3,20 @@ import { HTMLAttributes } from 'react'
 
 const Hr = ({ text, className, ...rest }: HrProps) => {
   return (
-    <div className="my-4 flex items-center gap-2">
-      {text && <span className="text-xs font-medium text-gray-700 dark:text-gray-300">{text}</span>}
-      <hr
-        className={'h-px flex-1 border-none bg-gray-700 dark:bg-gray-300' + addWithSpace(className)}
-        {...rest}
-      />
+    <div
+      className={
+        'my-4 flex items-center gap-2 text-gray-700 dark:text-gray-300' + addWithSpace(className)
+      }
+      {...rest}
+    >
+      {text && <span className="text-xs font-medium">{text}</span>}
+      <hr className="h-px flex-1 border-none bg-current" />
     </div>
   )
 }
 
 export type HrProps = {
   text?: string
-} & HTMLAttributes<HTMLHRElement>
+} & HTMLAttributes<HTMLDivElement>
 
 export default Hr
