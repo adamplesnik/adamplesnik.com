a05464a26b111605c2d37cc6294127f91d73d75f
Adam Plesnik <adam.plesnik@expersoft.com>
2024-06-21 23:30:22 +0200

    Update skills

--- src/content/SkillsTile.tsx
+++ src/content/SkillsTile.tsx
@@ -9,32 +9,38 @@ const skills = [
   'Theme refactoring',
   'Figma',
   'React',
+  'CSS animations',
   'Framer motion',
   'Vue.js',
 ]
 
 const SkillsTile = () => {
   return (
-    <div>
-      <Heading size={3}>Skills</Heading>
-      <Paragraph>
-        I specialize in <Em>nicer parts of the front end universe</Em>. I write CSS and front end
-        components for React, Vue.js, Angular and even Vaadin. My experience ranges from
-        implementing Figma-based prototypes from scratch to refactoring huge projects.
-      </Paragraph>
-      <Paragraph>
-        I have a <Em>passion for atomic components with atomic CSS</Em>, clean and meaningful UIs,
-        micro interactions.
-      </Paragraph>
-      <Heading size={4}>What I love to do, ordered by seniority</Heading>
-      <ul className="flex flex-wrap gap-2">
-        {skills.map((skill, i) => (
-          <li key={i} className="rounded bg-gradient-to-tr from-zinc-800 to-stone-800 p-2">
-            {skill}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <Heading size={3}>Skills</Heading>
+        <Paragraph>
+          I specialize in <Em>nicer parts of the front end universe</Em>. I write CSS and front end
+          components for React, Vue.js, Angular and even Vaadin. My experience ranges from
+          implementing Figma-based prototypes from scratch to refactoring huge projects.
+        </Paragraph>
+        <Paragraph>
+          I have a <Em>passion for atomic components with atomic CSS</Em>, clean and meaningful UIs,
+          micro interactions.
+        </Paragraph>
+      </div>
+      <div>
+        <Heading size={3}>What I love to do, ordered by seniority</Heading>
+        <ul className="flex flex-wrap gap-3">
+          {skills.map((skill, i) => (
+            <li key={i} className="flex items-center gap-3">
+              {skill}
+              <span className="block h-px w-8 bg-zinc-700" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -6,7 +6,7 @@ const Page = ({ children }: PropsWithChildren<PageProps>) => {
   return (
     <div className="flex w-screen justify-center">
       <PageBackground />
-      <div className="relative mb-12 flex w-full max-w-screen-md flex-col gap-8 overflow-hidden p-8 pt-12 md:gap-12">
+      <div className="relative mb-12 flex w-full max-w-screen-md flex-col gap-8 overflow-hidden p-8 pt-12 md:gap-16">
         {children}
         <Footer />
       </div>
