2a092e6b52ed8059fc311659d7d539fa41b0ee80
Adam Plesník <adam.plesnik@gmail.com>
2024-06-22 12:30:54 +0200

    Fix lovetodo on mobile

--- src/content/LoveToDo.tsx
+++ src/content/LoveToDo.tsx
@@ -16,7 +16,7 @@ const LoveToDo = () => {
   return (
     <div>
       <Heading size={3}>What I love to do</Heading>
-      <ul className="mb-4 grid grid-flow-col gap-3 sm:grid-rows-5">
+      <ul className="mb-4 grid sm:grid-flow-col gap-3 sm:grid-rows-5">
         {skills.map((skill, i) => (
           <li key={i} className="flex items-center gap-2">
             <span className="block h-px w-6 bg-zinc-600" />
