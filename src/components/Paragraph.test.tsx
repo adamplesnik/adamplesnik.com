94c816222136665713247aa556ec0452963e288e
Adam Plesnik <adam.plesnik@gmail.com>
2024-10-01 11:19:58 +0200

    Update tests

--- src/components/Paragraph.test.tsx
+++ src/components/Paragraph.test.tsx
@@ -3,30 +3,16 @@ import renderer from 'react-test-renderer'
 import { expect, test } from 'vitest'
 import Paragraph from './Paragraph'
 
-test('Small paragraph', () => {
-  const component = renderer.create(<Paragraph size="small">Small paragraph</Paragraph>)
-  let tree = toJson(component)
-  expect(tree).toMatchSnapshot()
-})
-
 test('Regular paragraph', () => {
-  const component = renderer.create(<Paragraph size="regular">Regular paragraph</Paragraph>)
-  let tree = toJson(component)
-  expect(tree).toMatchSnapshot()
-})
-
-test('Large paragraph', () => {
-  const component = renderer.create(<Paragraph size="large">Large paragraph</Paragraph>)
-  let tree = toJson(component)
+  const component = renderer.create(<Paragraph>Regular paragraph</Paragraph>)
+  const tree = toJson(component)
   expect(tree).toMatchSnapshot()
 })
 
 test('Regular paragraph with class name', () => {
   const component = renderer.create(
-    <Paragraph size="regular" className="test-class">
-      Regular paragraph with class name
-    </Paragraph>
+    <Paragraph className="test-class">Paragraph with class name</Paragraph>
   )
-  let tree = toJson(component)
+  const tree = toJson(component)
   expect(tree).toMatchSnapshot()
 })
