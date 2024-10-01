d0985216c391622334e597493c9ab2274f8919b4
Adam Plesnik <adam.plesnik@gmail.com>
2024-10-01 09:06:19 +0200

    Remove Link icon

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,49 +1,20 @@
-import { motion } from 'framer-motion'
-import { LucideIcon } from 'lucide-react'
-import { PropsWithChildren, useState } from 'react'
+import { PropsWithChildren } from 'react'
 
-const Link = ({
-  Icon = undefined,
-  href,
-  children,
-  target = '_blank',
-}: PropsWithChildren<LinkProps>) => {
-  const [anim, setAnim] = useState(false)
-
+const Link = ({ href, children, target = '_blank' }: PropsWithChildren<LinkProps>) => {
   return (
-    <div
-      className="relative flex items-center"
-      onMouseEnter={() => setAnim(true)}
-      onMouseLeave={() => setAnim(false)}
+    <a
+      href={href}
+      target={target}
+      className="flex items-center text-zinc-900 hover:text-zinc-700 print:text-zinc-700"
     >
-      {Icon ? (
-        <motion.div
-          initial={{ scale: 1 }}
-          animate={{
-            scale: anim ? 1.3 : 1,
-          }}
-          transition={{ duration: 0.72, type: 'spring' }}
-        >
-          <Icon className="size-7 pr-2 text-pink-600 opacity-70" strokeWidth={1.8} />
-        </motion.div>
-      ) : (
-        ''
-      )}
-      <a
-        href={href}
-        target={target}
-        className="text-zinc-500 hover:text-zinc-700 print:text-zinc-700"
-      >
-        {children}
-      </a>
-    </div>
+      {children}
+    </a>
   )
 }
 
 export interface LinkProps {
   children: PropsWithChildren
   href?: string
-  Icon?: LucideIcon | undefined
   target?: string
 }
 
