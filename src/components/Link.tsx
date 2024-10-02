cac487fa4860b38b817dfed80fb02eb22b937d05
Adam Plesnik <adam.plesnik@gmail.com>
2024-10-02 14:33:03 +0200

    Add aria-label (and any other prop) to Link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,11 +1,22 @@
+import clsx from 'clsx'
 import { AnchorHTMLAttributes } from 'react'
 
-const Link = ({ href, children, target = '_blank' }: AnchorHTMLAttributes<HTMLAnchorElement>) => {
+const Link = ({
+  href,
+  children,
+  target = '_blank',
+  className,
+  ...props
+}: AnchorHTMLAttributes<HTMLAnchorElement>) => {
   return (
     <a
       href={href}
       target={target}
-      className="flex items-center text-zinc-700 underline decoration-zinc-600 hover:text-black hover:decoration-zinc-800 dark:text-zinc-300 dark:decoration-zinc-500 dark:hover:text-zinc-50 dark:hover:decoration-zinc-100"
+      className={clsx(
+        'flex items-center text-zinc-700 underline decoration-zinc-600 hover:text-black hover:decoration-zinc-800 dark:text-zinc-300 dark:decoration-zinc-500 dark:hover:text-zinc-50 dark:hover:decoration-zinc-100',
+        className
+      )}
+      {...props}
     >
       {children}
     </a>
