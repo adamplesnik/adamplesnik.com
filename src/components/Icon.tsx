70a18eb5116a52bfaa0ba0d9d5204f37fb6bab7b
Adam Plesnik <adam.plesnik@gmail.com>
2024-10-02 14:02:38 +0200

    Update Footer, Icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import { LucideIcon } from 'lucide-react'
 
 const Icon = ({ Icon, className }: { Icon: LucideIcon; className?: string }) => {
-  return <Icon className={clsx('size-5 translate-y-1', className)} />
+  return <Icon className={clsx('size-5 translate-y-1', className)} strokeWidth={1.5} />
 }
 
 export default Icon
