86f5718dda1086879504fd4d452974d801b21163
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-12 13:46:11 +0100

    Remove badge from Kolbord

--- src/content/WorkKolbord.tsx
+++ src/content/WorkKolbord.tsx
@@ -2,14 +2,6 @@ import Link from '@/components/Link'
 import Paragraph from '@/components/Paragraph'
 import Tile from '@/components/Tile'
 
-const Badge = () => {
-  return (
-    <span className="flex h-fit -rotate-6 rounded bg-zinc-800 p-1 text-xs text-white dark:border dark:border-zinc-500 dark:bg-gray-700 print:bg-zinc-800">
-      Beta
-    </span>
-  )
-}
-
 const Links = () => {
   return (
     <>
@@ -21,7 +13,7 @@ const Links = () => {
 
 const WorkKolbord = () => {
   return (
-    <Tile title="Kolbord: the smoothest space booking" badge={<Badge />} links={<Links />} sub>
+    <Tile title="Kolbord: the smoothest space booking" links={<Links />} sub>
       <Paragraph>
         Opinionated, free to use, and open source app for two-click reservation of anything.
         Prototyped in Figma, coded in React, Payload CMS, with user management in Clerk.
