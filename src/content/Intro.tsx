ff3854e99d38a838fbe3a6c6c1196974ab89f6bd
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-13 21:24:52 +0100

    Simpler intro

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -1,22 +1,14 @@
 import Em from '@/components/Em'
-import Link from '@/components/Link'
 import Paragraph from '@/components/Paragraph'
 import Tile from '@/components/Tile'
 
 const Intro = () => {
   return (
     <Tile>
-      <Paragraph>Design engineer, Bratislava, Slovakia.</Paragraph>
-      <Paragraph>
+      <Paragraph className="max-w-xl text-3xl">
         I create, polish, and improve user interfaces by clicking in <Em>Figma</Em> and coding in{' '}
-        <Em>React</Em>. I have a passion for atomic components with <Em>Tailwind CSS</Em>, clean and
-        meaningful UIs, and refactoring.
+        <Em>React</Em>
       </Paragraph>
-      <div className="hidden gap-4 print:flex">
-        <Link href="https://adamplesnik.com">adamplesnik.com</Link>
-        <Link href="mailto:adam@adamplesnik.com">adam@adamplesnik.com</Link>
-        <Link href="https://github.com/adamplesnik">github.com/adamplesnik</Link>
-      </div>
     </Tile>
   )
 }
