76f4a22d3dc4d59986df92ccf1cd1c785e67ab3a
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-14 17:39:40 +0100

    Delete Header, Layout returns only Outlet

--- src/partials/Layout.tsx
+++ src/partials/Layout.tsx
@@ -1,15 +1,7 @@
 import { Outlet } from 'react-router-dom'
-import Header from './Header'
 
 const Layout = () => {
-  return (
-    <>
-      <Header />
-      <div className="flex w-full flex-col gap-24" role="main">
-        <Outlet />
-      </div>
-    </>
-  )
+  return <Outlet />
 }
 
 export default Layout
