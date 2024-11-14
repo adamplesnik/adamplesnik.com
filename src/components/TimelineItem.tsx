bf4fb60fb4b76bbb8a1d27f9a3178e7073d0ae7b
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-14 23:45:38 +0100

    Update timeline

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { Plus } from 'lucide-react'
+import { MoveRight } from 'lucide-react'
 import Paragraph from './Paragraph'
 
 const TimelineItem = ({
@@ -13,7 +13,7 @@ const TimelineItem = ({
 }) => {
   return (
     <div className={clsx('flex gap-2 border-l border-l-black', className)}>
-      <Plus strokeWidth={1.5} className="ml-1" />
+      <MoveRight strokeWidth={1.5} className="-ml-px" />
       <div>
         <div className="font-serif italic">{year}</div>
         <Paragraph className="pb-8">{desc}</Paragraph>
--- src/content/AboutMe.tsx
+++ src/content/AboutMe.tsx
@@ -3,16 +3,16 @@ import TimelineItem from '@/components/TimelineItem'
 
 const AboutMe = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-bl from-[#EBAF25] to-[#FEC84B] to-30% p-12">
-      <Paragraph>
+    <div className="flex min-h-screen flex-col gap-12 bg-gradient-to-bl from-[#EBAF25] to-[#FEC84B] to-30% p-12 md:flex-row">
+      <Paragraph className="flex-1 md:sticky md:top-12">
         I spend lot of quality time with my family—or alone if needed—preferably while mountain
         biking, traveling or both. I also shoot pictures and I love to read.
       </Paragraph>
-      <div className="mt-12 flex flex-col">
+      <div className="flex flex-1 flex-col">
         <TimelineItem
           year={'A long time ago'}
           desc="Han made the Kessel run in 12 parsecs."
-          className="pb-48"
+          className="pb-72"
         />
         <TimelineItem year={1985} desc="Born" className="pb-24" />
         <TimelineItem
@@ -24,7 +24,7 @@ const AboutMe = () => {
         <TimelineItem year={'2011–2012'} desc="Web designer in Websupport." />
         <TimelineItem year={'2013–now'} desc="Design engineer in Expersoft Technologies." />
         <TimelineItem year={2014} desc="Married." />
-        <TimelineItem year={'2015 & 2021'} desc="Kids." className="pb-72" />
+        <TimelineItem year={'2015 & 2021'} desc="Kids." className="pb-80" />
         <TimelineItem year={12067} desc="Hari will develop psychohistory." className="pb-80" />
         <TimelineItem year={23354} desc="Paul will become overly sensitive." />
       </div>
