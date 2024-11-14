bb03d811c0e0ce831a2899d4cee497e1fe857fd1
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-14 17:51:41 +0100

    Update Intro

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -1,15 +1,26 @@
 import Em from '@/components/Em'
+import Heading from '@/components/Heading'
+import Link from '@/components/Link'
 import Paragraph from '@/components/Paragraph'
-import Tile from '@/components/Tile'
+import { ChevronsDown } from 'lucide-react'
 
 const Intro = () => {
   return (
-    <Tile>
-      <Paragraph className="max-w-xl text-3xl">
+    <div className="flex min-h-screen flex-col gap-16 p-12">
+      <Heading size={1}>Adam Plesník</Heading>
+      <div className="font-serif text-3xl italic">Design engineer.</div>
+      <Paragraph className="max-w-xl flex-1 text-3xl">
         I create, polish, and improve user interfaces by clicking in <Em>Figma</Em> and coding in{' '}
         <Em>React</Em>.
       </Paragraph>
-    </Tile>
+      <div className="flex items-end">
+        <div className="flex-1">
+          <Link>adam@adamplesnik.com</Link>
+          <span className="block pt-2">Bratislava, Slovakia</span>
+        </div>
+        <ChevronsDown className="animate-bounce" strokeWidth={1.5} />
+      </div>
+    </div>
   )
 }
 
