ada3977bbbae67d4af5f3beed7137b37c10e5d7d
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-15 17:09:29 +0100

    Update Timeline, CV

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -1,23 +1,23 @@
 import clsx from 'clsx'
-import { MoveRight } from 'lucide-react'
 import Paragraph from './Paragraph'
 
 const TimelineItem = ({
   year,
   desc,
   className,
+  bold,
+  right,
 }: {
   year: number | string
   desc: string
   className?: string
+  bold?: boolean
+  right?: boolean
 }) => {
   return (
-    <div className={clsx('flex gap-2 border-l border-l-black', className)}>
-      <MoveRight strokeWidth={1.5} className="-ml-px" />
-      <div>
-        <div className="font-serif italic">{year}</div>
-        <Paragraph className="pb-8">{desc}</Paragraph>
-      </div>
+    <div className={clsx('flex gap-4 border-b border-b-black/20 py-4', className)}>
+      <div className="w-32 font-serif italic sm:w-40 md:w-52">{year}</div>
+      <Paragraph className={clsx(bold && 'font-semibold', right && 'text-right')}>{desc}</Paragraph>
     </div>
   )
 }
--- src/content/AboutMe.tsx
+++ src/content/AboutMe.tsx
@@ -1,32 +1,33 @@
+import Heading from '@/components/Heading'
 import Paragraph from '@/components/Paragraph'
 import TimelineItem from '@/components/TimelineItem'
 
 const AboutMe = () => {
   return (
-    <div className="flex min-h-screen flex-col gap-12 bg-gradient-to-bl from-[#EBAF25] to-[#FEC84B] to-30% p-12 md:flex-row">
-      <Paragraph className="h-fit flex-1 md:sticky md:top-12">
+    <div className="min-h-screen gap-12 bg-gradient-to-bl from-[#EBAF25] to-[#FEC84B] to-30% p-12">
+      <Heading size={2}>CV</Heading>
+      <Paragraph className="mb-24 mt-8 max-w-xl">
         I spend lot of quality time with my family—or alone if needed—preferably while mountain
         biking, traveling or both. I also shoot pictures and I love to read.
       </Paragraph>
-      <div className="flex flex-1 flex-col">
-        <TimelineItem
-          year={'A long time ago'}
-          desc="Han made the Kessel run in 12 parsecs."
-          className="pb-72"
-        />
-        <TimelineItem year={1985} desc="Born" className="pb-24" />
+      <div className="flex max-w-6xl flex-col">
+        <TimelineItem year={'A long time ago'} desc="Han shot first!" className="mb-72" right />
+        <TimelineItem year={1985} desc="Born" className="mb-12" />
         <TimelineItem
           year={1999}
-          desc="Star Wars will never be a trilogy again."
-          className="pb-24"
+          desc="Star Wars will never be a trilogy again"
+          className="mb-16"
+          right
         />
-        <TimelineItem year={2010} desc="Master of Architecture." className="pb-6" />
-        <TimelineItem year={'2011–2012'} desc="Web designer in Websupport." />
-        <TimelineItem year={'2013–now'} desc="Design engineer in Expersoft Technologies." />
-        <TimelineItem year={2014} desc="Married." />
-        <TimelineItem year={'2015 & 2021'} desc="Kids." className="pb-80" />
-        <TimelineItem year={12067} desc="Hari will develop psychohistory." className="pb-80" />
-        <TimelineItem year={23354} desc="Paul will become overly sensitive." />
+        <TimelineItem year={'2008–2010'} desc="Freelance web designer" bold />
+        <TimelineItem year={2010} desc="Master of Architecture" />
+        <TimelineItem year={'2011–2012'} desc="Web designer in Websupport" bold />
+        <TimelineItem year={'2013–now'} desc="Design engineer in Expersoft Technologies" bold />
+        <TimelineItem year={2014} desc="Married" />
+        <TimelineItem year={'2015 & 2021'} desc="Kids" />
+        <TimelineItem year={'Occasionally'} desc="Portrait photographer" className="mb-80" bold />
+        <TimelineItem year={12067} desc="Hari will develop psychohistory" className="mb-80" right />
+        <TimelineItem year={23354} desc="Paul will become overly sensitive" right />
       </div>
     </div>
   )
