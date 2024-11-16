2bd269ef70826e6bacfac7dc402357b1ff09ee9d
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-16 17:32:44 +0100

    Use masonry css, add MasonryWrapper

--- src/partials/MasonryWrapper.tsx
+++ src/partials/MasonryWrapper.tsx
@@ -1,11 +1,20 @@
 import { PropsWithChildren } from 'react'
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import Masonry from 'react-masonry-css'
+
+const breakpointColumnsObj = {
+  default: 2,
+  639: 1,
+}
 
 const MasonryWrapper = ({ children }: PropsWithChildren) => {
   return (
-    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-      <Masonry>{children}</Masonry>
-    </ResponsiveMasonry>
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="-ml-4 flex w-auto"
+      columnClassName="pl-4 bg-clip-padding *:pb-4"
+    >
+      {children}
+    </Masonry>
   )
 }
 
