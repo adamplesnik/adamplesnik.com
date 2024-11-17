ad4c57e755d94185e8412f64cd9b0b9ce1c3cdc1
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-17 21:17:59 +0100

    Fix Heading 3 font weight

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,8 +2,6 @@ import clsx from 'clsx'
 import { HTMLAttributes } from 'react'
 
 const Heading = ({ size = 1, className, children }: TitleProps) => {
-  const defaultClasses = clsx('font-semibold', className)
-
   if (size === 1) {
     return (
       <h1 className={clsx(className, 'text-4xl font-semibold tracking-tight sm:text-5xl')}>
@@ -13,7 +11,7 @@ const Heading = ({ size = 1, className, children }: TitleProps) => {
   } else if (size === 2) {
     return <h2 className={clsx(className, 'font-serif text-2xl italic lg:text-3xl')}>{children}</h2>
   } else if (size === 3) {
-    return <h3 className={clsx(defaultClasses, 'font-serif text-2xl italic')}>{children}</h3>
+    return <h3 className={clsx(className, 'font-serif text-2xl italic')}>{children}</h3>
   }
 }
 
