86e391f4bdd3ac5962281541c97502abbb95a166
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-18 23:00:54 +0100

    Update Kolbord assets

--- src/content/WorkKolbord.tsx
+++ src/content/WorkKolbord.tsx
@@ -1,9 +1,11 @@
 import Link from '@/components/Link'
 import WorkTile from '@/components/WorkTile'
 import MasonryWrapper from '@/partials/MasonryWrapper'
-import kolbordDemo from './images/kolbord-demo.mp4'
 import kolbordFashion from './images/kolbord-fashion.jpg'
+import kolbordDemo from './images/kolbord-ipad.mp4'
 import kolbordMacbook from './images/kolbord-macbook.jpg'
+import kolbordSidebar from './images/kolbord-sidebar.png'
+import kolbordWatch from './images/kolbord-watch.png'
 
 const Links = () => {
   return (
@@ -22,16 +24,18 @@ const WorkKolbord = () => {
       text="Opinionated, free to use, and open source app reservation of anything. This is a one-man-project: I take care of everything from brand to code.
         Drawn and prototyped in Figma, coded in React, Payload CMS, with user management in Clerk. WIP as of November 2024."
     >
-      <div className="bg-fuchsia-100 p-4 sm:p-8">
-        <div className="relative mx-auto max-w-screen-lg overflow-hidden before:absolute before:bottom-0 before:right-0 before:top-0 before:z-10 before:w-2 before:bg-fuchsia-100">
+      <div className="bg-[#E2BEC0] p-4 sm:p-8">
+        <div className="relative mx-auto max-w-screen-lg overflow-hidden before:absolute before:bottom-0 before:right-0 before:top-0 before:z-10 before:w-2 before:bg-[#E2BEC0]">
           <video loop autoPlay playsInline muted className="-m-1">
             <source src={kolbordDemo} type="video/mp4"></source>
           </video>
         </div>
       </div>
       <MasonryWrapper>
+        <img src={kolbordSidebar} className="mb-4" alt="" />
+        <img src={kolbordWatch} className="mb-4" alt="" />
         <img src={kolbordFashion} className="mb-4" alt="" />
-        <img src={kolbordMacbook} alt="" />
+        <img src={kolbordMacbook} className="mb-4" alt="" />
       </MasonryWrapper>
     </WorkTile>
   )
