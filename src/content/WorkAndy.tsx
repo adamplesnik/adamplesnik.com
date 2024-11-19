7041d59b0aa15f0dc59d42d8b231d77361d85ef6
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-19 23:22:19 +0100

    Add andy NYC

--- src/content/WorkAndy.tsx
+++ src/content/WorkAndy.tsx
@@ -3,6 +3,7 @@ import MasonryWrapper from '@/partials/MasonryWrapper'
 import andyAlt from './images/andy-alt.jpg'
 import andyBadge from './images/andy-badge.jpg'
 import andyMain from './images/andy-main.jpg'
+import andyStreet from './images/andy-street.jpg'
 import andyWall from './images/andy-wall.jpg'
 
 const WorkAndy = () => {
@@ -10,6 +11,7 @@ const WorkAndy = () => {
     <WorkTile
       title="Andy Warhol museum logo"
       text="This is me dipping toes into logo waters. Nobody actually asked for it, it just struck me, after visiting the museum, that it should have a more solid identity, given the greatness of the king of pop art."
+      top={<img src={andyStreet} />}
     >
       <MasonryWrapper>
         <img src={andyMain} className="mb-4" />
