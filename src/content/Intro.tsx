c7135b91e36f02d6593addc4f054c0d570be5a3a
Adam Plesník <adam.plesnik@gmail.com>
2024-11-19 14:13:52 +0100

    Update Intro.tsx

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -8,9 +8,9 @@ const Intro = () => {
   return (
     <div className="flex min-h-svh w-full flex-col gap-16 px-6 pb-8 pt-12 sm:px-8 md:px-12 md:pb-12">
       <Heading size={1}>Adam Plesník</Heading>
-      <Heading size={2}>Design engineer.</Heading>
+      <Heading size={2}>Designer first, coder second.</Heading>
       <Paragraph className="flex-1 text-2xl">
-        I create, polish, and improve user interfaces by clicking in <Em>Figma</Em> and coding in{' '}
+        I create, polish, and improve user experiences and interfaces by clicking in <Em>Figma</Em> and coding in{' '}
         <Em>React</Em>.
       </Paragraph>
       <div className="flex flex-col items-end gap-4 sm:flex-row">
