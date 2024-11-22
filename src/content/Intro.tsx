dff753f53c434114bf58802b68da7933ff434cff
Adam Plesník <adam.plesnik@gmail.com>
2024-11-22 20:17:17 +0100

    Update Intro.tsx

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -7,10 +7,9 @@ import { ChevronsDown } from 'lucide-react'
 const Intro = () => {
   return (
     <div className="flex min-h-[calc(100svh_-_19rem)] w-full flex-col gap-8 px-6 pb-8 sm:gap-12 sm:px-8 md:gap-16 md:px-12 md:pb-12">
-      <Heading size={2}>Designer first, coder second</Heading>
+      <Heading size={2}>UX/UI designer</Heading>
       <Paragraph className="flex-1 text-2xl">
-        I create, polish, and improve user experiences and interfaces by clicking in <Em>Figma</Em>{' '}
-        and coding in <Em>React</Em>.
+        I create, polish, and improve user experiences and interfaces.
       </Paragraph>
       <div className="flex flex-col items-end gap-4 sm:flex-row">
         <div className="w-full flex-1">
