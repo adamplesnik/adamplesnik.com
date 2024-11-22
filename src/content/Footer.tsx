d1b27e36a8d4662d5b20a2a0895dc5c2c901074f
Adam Plesník <adam.plesnik@gmail.com>
2024-11-22 20:50:40 +0100

    Update Footer.tsx

--- src/content/Footer.tsx
+++ src/content/Footer.tsx
@@ -3,7 +3,7 @@ import Link from '@/components/Link'
 const Footer = () => {
   return (
     <div className="flex h-svh flex-col justify-end gap-1 px-6 py-8 sm:px-8 md:px-12 md:py-12">
-      <span className="mb-12">Designer first, coder second</span>
+      <span className="mb-12">UX/UI designer</span>
       Bratislava, Slovakia
       <Link href="mailto:adam@adamplesnik.com">adam@adamplesnik.com</Link>
       <Link href="https://github.com/adamplesnik">github.com/adamplesnik</Link>
