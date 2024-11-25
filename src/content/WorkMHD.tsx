24288b7956c25e54df745beb9e73bdf60315ffe5
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-25 15:29:58 +0100

    Add MHD final

--- src/content/WorkMHD.tsx
+++ src/content/WorkMHD.tsx
@@ -1,9 +1,12 @@
 import Paragraph from '@/components/Paragraph'
 import WorkTile from '@/components/WorkTile'
 import MasonryWrapper from '@/partials/MasonryWrapper'
-import andyAlt from './images/andy-alt.jpg'
-import andyMain from './images/andy-main.jpg'
+import mhdLang from './images/mhd-lang.mp4'
 import mhdMain from './images/mhd-main.jpg'
+import mhdTest from './images/mhd-test.jpg'
+import mhdTrio from './images/mhd-trio.png'
+import mhdVideo from './images/mhd-video.png'
+import mhd from './images/mhd.mp4'
 
 const More = () => {
   return (
@@ -21,11 +24,29 @@ const WorkMHD = () => {
       title="Ticket-buying redesign"
       more={<More />}
       text="A complete redesign of the ticket-buying experience for Bratislava public transport, from an completely new UX to a simpler, more intuitive ticket machine interface."
-      top={<img src={mhdMain} />}
+      top={<img src={mhdTrio} className="mb-4" />}
     >
       <MasonryWrapper>
-        <img src={andyMain} className="mb-4" />
-        <img src={andyAlt} className="mb-4" />
+        <div className="relative mx-auto mb-4 max-w-screen-xl">
+          <img src={mhdVideo} alt="" />
+          <div className="absolute left-[34.3%] top-[16.3%] w-[31.6%] overflow-hidden">
+            <video loop autoPlay playsInline muted className="-mt-[2px]">
+              <source src={mhd} type="video/mp4"></source>
+            </video>
+          </div>
+        </div>
+        <div className="relative mx-auto mb-4 max-w-screen-xl">
+          <img src={mhdVideo} alt="" />
+          <div className="absolute left-[34.3%] top-[16.3%] w-[31.6%] overflow-hidden">
+            <video loop autoPlay playsInline muted className="-mt-[2px]">
+              <source src={mhdLang} type="video/mp4"></source>
+            </video>
+          </div>
+        </div>
+        <img src={mhdMain} className="mb-4" />
+        <div className="mb-4 bg-[#BBE2DA]">
+          <img src={mhdTest} className="opacity-90" />
+        </div>
       </MasonryWrapper>
     </WorkTile>
   )
