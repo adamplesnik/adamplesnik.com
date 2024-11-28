6798bdb23e046e5ca7b0c6ff784d253dc6fda601
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-28 16:01:04 +0100

    Tuning

--- src/content/Footer.tsx
+++ src/content/Footer.tsx
@@ -1,15 +1,19 @@
 import Link from '@/components/Link'
+import Paragraph from '@/components/Paragraph'
 
 const Footer = () => {
   return (
-    <div className="flex h-svh flex-col justify-end gap-1 px-6 py-8 sm:px-8 md:px-12 md:py-12">
+    <Paragraph
+      big
+      className="flex h-svh flex-col justify-end gap-1 px-6 py-8 sm:px-8 md:px-12 md:py-12"
+    >
       <span className="font-medium">UX–UI designer</span>
       Bratislava, Slovakia
       <Link href="mailto:adam@adamplesnik.com" className="mt-12">
         adam@adamplesnik.com
       </Link>
       <Link href="https://github.com/adamplesnik">github.com/adamplesnik</Link>
-    </div>
+    </Paragraph>
   )
 }
 
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -7,14 +7,14 @@ import WorkPhoto from '@/content/WorkPhoto'
 
 const Work = () => {
   return (
-    <>
+    <div className="flex flex-col gap-16">
       <WorkMHD />
       <WorkKolbord />
       <WorkThankful />
       <WorkComparator />
       <WorkAndy />
       <WorkPhoto />
-    </>
+    </div>
   )
 }
 
