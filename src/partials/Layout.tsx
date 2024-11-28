247c81622a829dcab7fa00d370a5d24587adafbc
Adam Plesnik <adam.plesnik@expersoft.com>
2024-11-28 14:54:48 +0100

    Update Layout

--- src/partials/Layout.tsx
+++ src/partials/Layout.tsx
@@ -5,13 +5,13 @@ import { NavLink, Outlet, ScrollRestoration } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className="flex flex-col pt-4">
+    <div className="flex flex-col pt-8">
       <ScrollRestoration />
-      <div className="sticky top-0 z-50 mb-12 flex h-16 w-full items-end gap-8 bg-gradient-to-b from-white/80 px-6 sm:px-8 md:px-12 dark:from-zinc-800/80">
+      <div className="items-basline sticky top-0 z-50 mb-12 flex h-16 w-full gap-8 bg-gradient-to-b from-white/80 px-6 pt-4 sm:px-8 md:px-12 dark:from-zinc-800/80">
         <a href="/" className="cursor-pointer">
           <Heading size={1}>Adam Plesník</Heading>
         </a>
-        <div className="hidden flex-1 justify-end gap-4 sm:flex">
+        <div className="flex flex-1 justify-end gap-4">
           <NavLink
             className={({ isActive }) =>
               clsx('cursor-pointer text-2xl font-medium hover:underline', isActive && 'underline')
