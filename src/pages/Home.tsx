0b2aa250ae7774bc31abb6592ae094e93b4eba73
Adam Plesnik <adam.plesnik@expersoft.com>
2024-11-28 10:39:05 +0100

    Move header and layout to Layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import Heading from '@/components/Heading'
 import AboutMe from '@/content/AboutMe'
-import Footer from '@/content/Footer'
 import Intro from '@/content/Intro'
 import Skills from '@/content/Skills'
 import WorkAndy from '@/content/WorkAndy'
@@ -12,21 +10,7 @@ import WorkThankful from '@/content/WorkThankful'
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-48 pt-12">
-      <div className="sticky top-0 z-50 flex h-16 w-full items-end gap-8 bg-gradient-to-b from-white/80 px-6 sm:px-8 md:px-12 dark:from-zinc-800/80">
-        <Heading size={1}>Adam Plesník</Heading>
-        <div className="hidden flex-1 justify-end gap-4 sm:flex">
-          <a className="cursor-pointer hover:underline" href="#skills">
-            Skills
-          </a>
-          <a className="cursor-pointer hover:underline" href="#work">
-            Work
-          </a>
-          <a className="cursor-pointer hover:underline" href="#cv">
-            CV
-          </a>
-        </div>
-      </div>
+    <>
       <Intro />
       <Skills />
       <div id="work" className="relative top-[8.5rem]" />
@@ -38,8 +22,7 @@ const Home = () => {
       <WorkPhoto />
       <div id="cv" className="relative top-[7rem]" />
       <AboutMe />
-      <Footer />
-    </div>
+    </>
   )
 }
 
--- src/partials/Layout.tsx
+++ src/partials/Layout.tsx
@@ -1,7 +1,28 @@
+import Heading from '@/components/Heading'
+import Footer from '@/content/Footer'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-  return <Outlet />
+  return (
+    <div className="flex flex-col gap-48 pt-12">
+      <div className="sticky top-0 z-50 flex h-16 w-full items-end gap-8 bg-gradient-to-b from-white/80 px-6 sm:px-8 md:px-12 dark:from-zinc-800/80">
+        <Heading size={1}>Adam Plesník</Heading>
+        <div className="hidden flex-1 justify-end gap-4 sm:flex">
+          <a className="cursor-pointer hover:underline" href="#skills">
+            Skills
+          </a>
+          <a className="cursor-pointer hover:underline" href="#work">
+            Work
+          </a>
+          <a className="cursor-pointer hover:underline" href="#cv">
+            CV
+          </a>
+        </div>
+      </div>
+      <Outlet />
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
