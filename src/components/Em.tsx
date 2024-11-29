021bbf61485ca3c24bede34b80e57f5038a37d6e
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-29 22:01:46 +0100

    Delete more obsolete content

--- src/components/Em.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from 'clsx/lite'
-import { HTMLAttributes } from 'react'
-
-const Em = ({ children, className }: HTMLAttributes<HTMLSpanElement>) => {
-  return <span className={clsx('', className)}>{children}</span>
-}
-
-export default Em
