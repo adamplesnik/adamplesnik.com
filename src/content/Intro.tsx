c4aac2c2c1d680047445064b712b04ac2c82f2ce
Adam Plesnik <adam.plesnik@gmail.com>
2024-11-29 11:43:52 +0100

    Add hyphen

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <div className="flex w-full flex-col gap-8 px-6 pb-8 sm:gap-12 sm:px-8 md:gap-16 md:px-12 md:pb-12">
       <Paragraph big>
-        I am a remote friendly UX–UI designer creating nice and functional things across multiple
+        I am a remote-friendly UX–UI designer creating nice and functional things across multiple
         platforms.
       </Paragraph>
     </div>
