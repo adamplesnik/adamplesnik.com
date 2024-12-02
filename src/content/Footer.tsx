89c9faa6fc8a903b02e3cc2200507e2cf3664f23
Adam Plesník <adam.plesnik@gmail.com>
2024-12-02 07:30:34 +0100

    Update Footer.tsx

--- src/content/Footer.tsx
+++ src/content/Footer.tsx
@@ -4,7 +4,7 @@ import Paragraph from '@/components/Paragraph'
 const Footer = () => {
   return (
     <Paragraph className="mt-24 flex flex-col justify-end gap-1 bg-zinc-900 px-6 pb-8 pt-12 text-white sm:px-8 md:px-12">
-      Adam Plesník, UX–UI designer
+      Adam Plesník, graphic and UI designer
       <br />
       Bratislava, Slovakia
       <Link href="mailto:adam@adamplesnik.com" className="mt-12">
