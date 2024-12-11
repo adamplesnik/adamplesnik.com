1ad3b1d230a476d046769e296c3346cf1a2b80ec
Adam Plesnik <adam.plesnik@expersoft.com>
2024-12-11 14:57:49 +0100

    Update phrase about tech

--- src/content/phrases.ts
+++ src/content/phrases.ts
@@ -2,4 +2,4 @@ export const catchPhrase = `As a graphic designer, I bring value by providing so
 
 export const freeTime = `I value both creative and personal freedom, preferring remote, non-corporate, and flexible work. Family time is a non-negotiable, and I put high priority in my time off, enjoying mountain biking, hiking and travel—whether with family or solo when needed.`
 
-export const tech = `I am proficient in Figma, Adobe Photoshop, InDesign, Lightroom, and XD. I code in React+Typescript, TSX, Tailwind CSS, and I speak English and French.`
+export const tech = `I am proficient in Figma, Adobe Photoshop, InDesign, Lightroom, and XD, with experience in Rhino 3D and Blender. I code in React+Typescript, TSX, Tailwind CSS, and I speak English and French.`
