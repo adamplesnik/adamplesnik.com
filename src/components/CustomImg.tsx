faafe58ee313792060ae2d36656a17ab53e6abb9
Adam Plesnik <adam.plesnik@gmail.com>
2024-12-16 21:33:43 +0100

    Update CustomImg, add aspect ratio

--- src/components/CustomImg.tsx
+++ src/components/CustomImg.tsx
@@ -1,7 +1,7 @@
 import { ImgHTMLAttributes } from 'react'
 
-const CustomImg = ({ src, alt, loading = 'lazy' }: ImageType) => {
-  return <img src={src} loading={loading} alt={alt} />
+const CustomImg = ({ src, alt, loading = 'lazy', className }: ImageType) => {
+  return <img src={src} loading={loading} alt={alt} className={className} />
 }
 
 type ImageType = {} & ImgHTMLAttributes<HTMLImageElement>
--- src/components/WorkPreviewTile.tsx
+++ src/components/WorkPreviewTile.tsx
@@ -6,7 +6,7 @@ import Heading from './Heading'
 const WorkPreviewTile = ({ title, className, link, src }: WorkPreviewTileProps) => {
   return (
     <a href={link} className={clsx('w-full', className)}>
-      <CustomImg src={src} alt={title} />
+      <CustomImg src={src} alt={title} className="aspect-[2/1]" />
       <Heading size={2} className="mt-4 hover:underline">
         {title} &rarr;
       </Heading>
