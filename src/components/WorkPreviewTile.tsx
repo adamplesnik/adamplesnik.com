f554a86ee47fd987c92378b3dd7ad1d670e8fadf
Adam Plesnik <adam.plesnik@gmail.com>
2024-12-16 21:13:16 +0100

    Update WorkPreviewTile, remove separate previews

--- src/components/WorkPreviewTile.tsx
+++ src/components/WorkPreviewTile.tsx
@@ -1,11 +1,12 @@
 import { clsx } from 'clsx'
 import { HTMLAttributes } from 'react'
+import CustomImg from './CustomImg'
 import Heading from './Heading'
 
-const WorkPreviewTile = ({ children, title, className, link }: WorkPreviewTileProps) => {
+const WorkPreviewTile = ({ title, className, link, src }: WorkPreviewTileProps) => {
   return (
     <a href={link} className={clsx('w-full', className)}>
-      {children}
+      <CustomImg src={src} alt={title} />
       <Heading size={2} className="mt-4 hover:underline">
         {title} &rarr;
       </Heading>
@@ -16,6 +17,7 @@ const WorkPreviewTile = ({ children, title, className, link }: WorkPreviewTilePr
 type WorkPreviewTileProps = {
   title: string
   link: string
+  src: string | undefined
 } & HTMLAttributes<HTMLDivElement>
 
 export default WorkPreviewTile
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,25 @@
-import AndyPreview from '@/content/andy/AndyPreview'
+import WorkPreviewTile from '@/components/WorkPreviewTile'
+import andyPreview from '@/content/andy/images/andy-home.jpg'
 import Intro from '@/content/Intro'
-import KolbordPreview from '@/content/kolbord/KolbordPreview'
-import MartinPreview from '@/content/martin/MartinPreview'
-import MhdPreview from '@/content/mhd/MhdPreview'
-import VkjbPreview from '@/content/vkjb/VkjbPreview'
+import kolbordPreview from '@/content/kolbord/images/kolbord-home.jpg'
+import martinPreview from '@/content/martin/images/martin-home.jpg'
+import mhdPreview from '@/content/mhd/images/mhd-home.jpg'
+import vkjbPreview from '@/content/vkjb/images/vkjb-home.png'
 
 const Home = () => {
   return (
     <>
       <Intro />
       <div className="flex flex-col gap-8 p-6 sm:px-8 md:px-12">
-        <VkjbPreview />
-        <MartinPreview />
-        <MhdPreview />
-        <AndyPreview />
-        <KolbordPreview />
+        <WorkPreviewTile title="Public library in Košice" link="/work/library" src={vkjbPreview} />
+        <WorkPreviewTile title="Hospital in Martin" link="/work/martin" src={martinPreview} />
+        <WorkPreviewTile
+          title="Bratislava public transport"
+          link="/work/tickets"
+          src={mhdPreview}
+        />
+        <WorkPreviewTile title="Andy Warhol museum" link="/work/andy" src={andyPreview} />
+        <WorkPreviewTile title="Kolbord" link="/work/kolbord" src={kolbordPreview} />
       </div>
     </>
   )
