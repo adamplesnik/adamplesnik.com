be4fe2d7d1927ec2f12c8cd8ba10ee1317308c94
Adam Plesnik <adam.plesnik@expersoft.com>
2024-12-19 12:12:45 +0100

    Add backup catch phrase

--- src/content/phrases.ts
+++ src/content/phrases.ts
@@ -3,3 +3,6 @@ export const catchPhrase = `As a graphic designer, I bring value by providing so
 export const freeTime = `I value both creative and personal freedom, preferring remote, non-corporate, and flexible work. Family time is a non-negotiable, and I put high priority in my time off, enjoying mountain biking, hiking and travel—whether with family or solo when needed.`
 
 export const tech = `I am proficient in Figma, Adobe Photoshop, InDesign, Lightroom, and XD, with experience in Rhino 3D and Blender. I code in React+Typescript, TSX, Tailwind CSS, and I speak English and French.`
+
+/* If needed */
+export const catchPhrase2 = `I am a graphic designer with more than 15 years of experience with identity and brand design, UI design, and front-end coding. I believe I could be a valuable asset to your team especially thanks to my ability to seamlessly work in all these fields and ensure the identity is clean, functional and consistent no matter the medium.`
