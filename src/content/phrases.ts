abdffe2c2b2209f434b9c035c2a2589490f0db4a
Adam Plesník <adam.plesnik@gmail.com>
2024-12-22 12:23:21 +0100

    Update phrases.ts

--- src/content/phrases.ts
+++ src/content/phrases.ts
@@ -1,4 +1,4 @@
-export const catchPhrase = `As a graphic designer, I bring value by providing solutions that seamlessly work across multiple platforms—from branding, via customer and user experience, to UI.`
+export const catchPhrase = `As a graphic designer, I help brands and products create consistent, effective, and seamless experiences across platforms—from identity to UI.`
 
 export const freeTime = `I value both creative and personal freedom, preferring remote, non-corporate, and flexible work. Family time is a non-negotiable, and I put high priority in my time off, enjoying mountain biking, hiking and travel—whether with family or solo when needed.`
 
@@ -6,3 +6,4 @@ export const tech = `I am proficient in Figma, Adobe Photoshop, InDesign, Lightr
 
 /* If needed */
 export const catchPhrase2 = `I am a graphic designer with more than 15 years of experience with identity and brand design, UI design, and front-end coding. I believe I could be a valuable asset to your team especially thanks to my ability to seamlessly work in all these fields and ensure the identity is clean, functional and consistent no matter the medium.`
+export const catchPhraseOld = `As a graphic designer, I bring value by providing solutions that seamlessly work across multiple platforms—from branding, via customer and user experience, to UI.`
