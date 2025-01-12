a60d92424f2080396af5e759e9f760b1893a5ee5
Adam Plesník <adam.plesnik@gmail.com>
2025-01-12 17:47:01 +0100

    Update phrases.ts

--- src/content/phrases.ts
+++ src/content/phrases.ts
@@ -1,6 +1,6 @@
 export const catchPhrase = `As a graphic designer, I help brands and products create consistent, effective, and seamless experiences—from identity to UI.`
 
-export const freeTime = `I value both creative and personal freedom, preferring remote, non-corporate, and flexible work. Family time is a non-negotiable, and I put high priority in my time off, enjoying mountain biking, hiking and travel—whether with family or solo when needed.`
+export const freeTime = `I value both creative and personal freedom, preferring hybrid, purpose-driven, and flexible work. Family time is a non-negotiable, and I put high priority in my time off, enjoying mountain biking, hiking and travel—whether with family or solo when needed.`
 
 export const tech = `I am proficient in Figma, Adobe Photoshop, InDesign, Lightroom, and XD, with experience in Rhino 3D and Blender. I code in React+Typescript, TSX, Tailwind CSS, and I speak English and French.`
 
