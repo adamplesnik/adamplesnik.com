7572acf6ae3dd54b0be96309fa7877ee1631749a
Adam Plesnik <adam.plesnik@gmail.com>
2025-01-15 20:30:35 +0100

    Update mobile spacing

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -4,7 +4,7 @@ import { catchDetail, catchPhrase } from './phrases'
 const Intro = () => {
   return (
     <>
-      <Paragraph big className="px-6 pb-8 sm:px-8 md:px-12 md:pb-12">
+      <Paragraph big className="px-6 pb-12 sm:px-8 md:px-12">
         {catchPhrase}
       </Paragraph>
       <Paragraph big className="px-6 pb-8 sm:px-8 md:px-12 md:pb-12">
