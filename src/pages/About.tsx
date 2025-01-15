8e61df645fadd4657bd14d6f5159811b7dbce563
Adam Plesnik <adam.plesnik@expersoft.com>
2025-01-15 12:59:28 +0100

    Update spacing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,16 +5,16 @@ import { catchDetail, catchPhrase, freeTime, tech } from '@/content/phrases'
 const About = () => {
   return (
     <>
-      <Paragraph big className="mb-16 px-6 sm:px-8 md:px-12">
+      <Paragraph big className="mb-12 px-6 sm:px-8 md:px-12">
         {catchPhrase}
       </Paragraph>
-      <Paragraph big className="mb-16 px-6 sm:px-8 md:px-12">
+      <Paragraph big className="mb-12 px-6 sm:px-8 md:px-12">
         {catchDetail}
       </Paragraph>
-      <Paragraph big className="mb-16 px-6 sm:px-8 md:px-12">
+      <Paragraph big className="mb-12 px-6 sm:px-8 md:px-12">
         {tech}
       </Paragraph>
-      <Paragraph big className="mb-16 px-6 sm:px-8 md:px-12">
+      <Paragraph big className="mb-24 px-6 sm:px-8 md:px-12">
         {freeTime}
       </Paragraph>
       <div className="-mb-24 flex flex-col gap-2 bg-cv-light p-6 sm:p-8 md:p-12 dark:bg-cv-dark">
