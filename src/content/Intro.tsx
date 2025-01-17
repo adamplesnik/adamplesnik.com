9ebc8365cfaa39549914907d43b12a6cbab7c419
Adam Plesnik <adam.plesnik@expersoft.com>
2025-01-17 09:34:25 +0100

    Update fades

--- src/content/Intro.tsx
+++ src/content/Intro.tsx
@@ -1,16 +1,17 @@
 import Paragraph from '@/components/Paragraph'
+import { Fade } from 'react-awesome-reveal'
 import { catchDetail, catchPhrase } from './phrases'
 
 const Intro = () => {
   return (
-    <>
+    <Fade triggerOnce cascade damping={0.09}>
       <Paragraph big className="px-6 pb-12 sm:px-8 md:px-12">
         {catchPhrase}
       </Paragraph>
       <Paragraph big className="px-6 pb-8 sm:px-8 md:px-12 md:pb-12">
         {catchDetail}
       </Paragraph>
-    </>
+    </Fade>
   )
 }
 
